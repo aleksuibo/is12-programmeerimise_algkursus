3e1949a765b4baa75d7664cbe40dc20cf731dc57
aleks <aleks@aleks-Inspiron-N5110.(none)>
2013-09-17 16:06:18 +0300

    minu esimene programm

--- a.py
+++ a.py
@@ -4,3 +4,25 @@ print 1*2
 print 10/2
 print 2//10
 print 10%9
+print -5
+print +5
+print abs(1)
+print abs(-1)
+print int(5.945)
+print long(55.655464)
+print float(55)
+print "abc". capitalize()
+print "abc". isalnum()
+print "0". isalpha()
+print "ab[c]". islower()
+print ("{:.2f}".format(3.1415925));
+print ("{:+.2f}".format(3.1415925));
+print ("{:+.2f}".format(-1.));
+print ("{:.0f}".format(2.71828));
+print ("{:0>2d}".format(5));
+print ("{:x<4d}".format(5));
+print ("{:x<4d}".format(10));
+print ("{:,}".format(1000000));
+print ("{:.2%}".format(0.25));
+
+
