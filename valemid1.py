bb71a685a8b347bcb0ccb3cb65717f90eca175df
aleks <aleks@aleks-Inspiron-N5110.(none)>
2013-09-24 22:02:20 +0300

    valemid

---
+++ valemid1.py
@@ -0,0 +1,72 @@
+# kytusekulu 100km kohta arvutamine 
+l= 20
+km= 10
+kytusekulu= (l*100)/km 
+print kytusekulu
+
+# Teepikkuse leidmine
+v=10 #km/h
+t=1  #tundi
+s=v*t  #km
+print s
+
+
+# Energia valem
+
+m=10 #kg
+c=3*10**8
+E=m*c**2
+print E
+
+# Rohk p=F/S
+
+F=10 #J
+S=10 #m**2
+p=F/S
+print p
+
+# Ruudu pindala
+
+a=5   #ruudu kulje pikkus
+P=a**2
+print P
+
+# Ruudu umbermoot
+
+a=5
+P=4*a
+print P
+
+# Eurode teisendamine Kroonideks
+
+e=10  #eurod
+A=e*15.6
+print "{0} {1}" .format(A,"krooni")
+
+
+# Summa ruudu valem
+a=1
+b=2
+Vastus=(a+b)**2 
+print Vastus
+
+	
+# Vahe ruudu valem
+a=1
+b=2
+Vastus=(a-b)**2
+print Vastus
+
+
+# kolmnurga pindala
+
+a=3
+h=4
+
+S=a*h/2
+print S
+
+
+
+
+
