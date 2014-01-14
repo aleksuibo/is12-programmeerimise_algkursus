08918e6cba38f9fdc8e2a00379f4f0564691f29b
aleksuibo <aleks547@hotmail.com>
2014-01-14 18:23:09 +0200

    Create parandus1.py

---
+++ parandus1.py
@@ -0,0 +1,15 @@
+a = 1
+while a<5:
+        
+        b=0
+        while b<a:
+                print "$",
+                b=b+1
+		
+        b=5
+        while a<b:
+                print "#",
+                b=b-1
+                
+        print ""
+        a=a+1 
